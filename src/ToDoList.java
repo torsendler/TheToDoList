c179756e74cb4cca279725c024276e45319beaba
Torsten-Eric Sendler <t.-e._sendlerutlook.de>
2024-10-16 08:48:13 +0200

    Klasse ToDoList ausgearbeitet

--- src/ToDoList.java
+++ src/ToDoList.java
@@ -1,3 +1,14 @@
-public class ToDoList {
+import java.time.LocalDateTime;
 
+public class ToDoList {
+   String aufgabe;
+   LocalDateTime ldt = LocalDateTime.now();
+   boolean erledigt;
+   int prio;
+
+    public ToDoList(String aufgabe, boolean erledigt, int prio) {
+        this.aufgabe = aufgabe;
+        this.erledigt = erledigt;
+        this.prio = prio;
+    }
 }
